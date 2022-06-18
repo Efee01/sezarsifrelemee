35489a5970a5df9b15d4c8ea31ff162084103928
Efee01 <101813015+Efee01@users.noreply.github.com>
2022-06-19 02:51:16 +0300

    Add files via upload

---
+++ sezarwithoffset/Sezar/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Sezar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.cumlelabeli = new System.Windows.Forms.TextBox();
+            this.sifrelemelabeli = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(391, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ŞİFRELE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cumlelabeli
+            // 
+            this.cumlelabeli.Location = new System.Drawing.Point(201, 13);
+            this.cumlelabeli.Name = "cumlelabeli";
+            this.cumlelabeli.Size = new System.Drawing.Size(184, 23);
+            this.cumlelabeli.TabIndex = 1;
+            this.cumlelabeli.TextChanged += new System.EventHandler(this.cumlelabeli_TextChanged);
+            // 
+            // sifrelemelabeli
+            // 
+            this.sifrelemelabeli.Location = new System.Drawing.Point(201, 38);
+            this.sifrelemelabeli.Name = "sifrelemelabeli";
+            this.sifrelemelabeli.Size = new System.Drawing.Size(184, 23);
+            this.sifrelemelabeli.TabIndex = 2;
+            this.sifrelemelabeli.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cümlenizi Giriniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifrelenmiş veya çözülmüş Hali";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(391, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "şifreyi çöz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 130);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sifrelemelabeli);
+            this.Controls.Add(this.cumlelabeli);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox cumlelabeli;
+        private System.Windows.Forms.TextBox sifrelemelabeli;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ sezarwithoffset/Sezar/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sezar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cumle = "";
+            cumle =cumlelabeli.Text;//sifrelenecek mesajı textten kullanıcıdan aldırdım
+            char[] chars = cumle.ToCharArray();//burda yazılan cümleyi chara çevirdim
+            foreach (char i in chars)
+            {
+                
+               sifrelemelabeli.Text += Convert.ToChar(i + 3).ToString();
+                //chara dönüşen 3 birim ötelenmiş şifreyi
+               //diğer texte yazdırsın diye foreach döngüsü oluşturdum 
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string cozum = "";
+            cozum = cumlelabeli.Text;//sifrelenmiş mesajı textten kullanıcıdan aldırdım
+            char[] chars = cozum.ToCharArray();
+            foreach (char i2 in chars)
+            {
+                
+                sifrelemelabeli.Text += Convert.ToChar(i2 -3).ToString();
+                //3 birim ötelenen şifreyi geri çözmesi için 3 azaltıp
+                //asıl cümleyi bulması için tekrar foreach döngüsü oluşturdum 
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cumlelabeli_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
